6d0fc099368bd7b4a552796b269793ccdfc09ab2
Sean Richter <sean.richter04@outlook.com>
2019-12-09 20:56:38 -0600

    Updates

---
+++ precache-manifest.03477e1effc6e201132e33ae627138cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a6a7dabc222b52675c0fb5c575fac5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dd34beee74575983dbc3",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "de9e636173876b825a4b",
+    "url": "/clicky-game/static/js/2.ab983b68.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.ab983b68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd34beee74575983dbc3",
+    "url": "/clicky-game/static/js/main.8b3e4a88.chunk.js"
+  },
+  {
+    "revision": "6c166bcd5bcf3bb0e1d8",
+    "url": "/clicky-game/static/js/runtime-main.8b428ba5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
