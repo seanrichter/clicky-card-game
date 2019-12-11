3f007214833d7bc40faf48545b2805c4ab00495b
Sean Richter <sean.richter04@outlook.com>
2019-12-10 20:17:34 -0600

    updated title in header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = props => (
     <div id="headerContainer" className="container">
-        <div id="title">Suit Up Clicky Game</div>
+        <div id="title">Clicky Card Game</div>
         <div id="directions">Don't click the same image twice!</div>
         <div id="scoreDiv">Score: {props.score}</div>
     </div>
